da193085725828a996432b2968320e5ed1326f3b
Ishita <ishitagops12@gmail.com>
2022-02-13 21:45:13 +0530

    string change

--- whitespace_remove.py
+++ whitespace_remove.py
@@ -8,7 +8,7 @@ def eliminate_spaces(my_string):
     
 def startpy():
 
-    str1 = "\n        i am  a  gamer"
+    str1 = "\n        i love pizza and pasta"
     my_string_1= eliminate_spaces(str1)   
     print(my_string_1)
 
