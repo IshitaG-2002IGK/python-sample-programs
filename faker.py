d2f49d8e192fbc416b18e8fbbdd3344d51f76791
Ishita <ishitagops12@gmail.com>
2022-02-13 21:38:32 +0530

    fake name generation tryout

---
+++ faker.py
@@ -0,0 +1,28 @@
+'''
+Created on 
+Course work: 
+@author: Ishita
+Source:
+    
+'''
+
+from faker import Faker
+ 
+fake=Faker()
+
+def generate_random_names():
+
+    random_names = []
+    for x in range(5) :
+        random_names.append(fake.name())
+    return  random_names  
+    
+
+def startpy():
+
+    random_name_list = generate_random_names()   
+    print(random_name_list)
+
+
+if __name__ == '__main__':
+    startpy()
